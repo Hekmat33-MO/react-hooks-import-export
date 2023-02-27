cc06fe154f51bab42f64301b8372bccda92a80e8
Hekmat Mohammed <heka332020@gmail.com>
2023-02-27 19:56:06 +0300

    DONE

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,22 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as AllFuncions from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+    howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+    return <h1 > Colorado State Parks! < /h1>;
+    console.log(AllFuncions.trees);
+    AllFuncions.wildlife();
+    return ( <
+        div >
+        <
+        h1 > Colorado State Parks! < /h1> <
+        MesaVerde / >
+        <
+        /div>
+    );
 }
+
+export default ColoradoStateParks;
